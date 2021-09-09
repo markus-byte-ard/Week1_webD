cfa36fb80b3369b160187d4804899ca666321dde
markus-byte-ard <61155395+markus-byte-ard@users.noreply.github.com>
2021-09-09 21:26:19 +0300

    Finished tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
+if (document.readyState !== "loading") {
+  initialize();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready");
+    initialize();
+  });
+}
+
+function initialize() {
+  const addCommentButton = document.getElementById("add-comment");
+  const RemoveCommentsButton = document.getElementById("remove-comments");
+  const commentList = document.getElementById("comment-list");
+
+  addCommentButton.addEventListener("click", function () {
+    const commentText = document.getElementById("comment-text");
+
+    let newListItem = document.createElement("li");
+    if (commentText.value !== "") {
+      newListItem.innerHTML = commentText.value;
+      commentList.appendChild(newListItem);
+    }
+  });
+
+  RemoveCommentsButton.addEventListener("click", function () {
+    let confirmation = confirm("Are you sure ?");
+    if (confirmation === true) {
+      while (commentList.hasChildNodes())
+        commentList.removeChild(commentList.lastChild);
+    }
+  });
+}
+
+/*document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
@@ -8,3 +41,4 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+*/
